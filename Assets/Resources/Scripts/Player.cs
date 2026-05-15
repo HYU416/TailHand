459a51e4d67ae490373e01e3e3e47685ddd23f65
OSUSIMANN <ryuuki20040818@icloud.com>
2026-05-15 09:49:10 +0900

    feature/6-impl-player-move
    ギアの追加
    加速・減速の追加

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,14 +6,38 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 5.0f; //Playerの速さ
     public float rotateSpeed = 10.0f; //Playerの回転の速さ
+    public float currentSpeed = 0f;
+    public float deceleration = 3.0f;// 1秒あたりの減速量
+
+    [Header("各ギアの最低速度")]
+    [SerializeField]
+    private List<float> gearSpeeds = new List<float>()
+    {
+        1.0f,3.0f,6.0f,10.0f
+    };
+
+    [Header("ギア上昇に必要な秒数")]
+    [SerializeField]
+    private List<float> gearUpTimes = new List<float>()
+    {
+        50.0f,30.0f,20.0f
+    };
 
     Rigidbody rb;
     Vector2 moveInput;
     bool movebutton;
+    bool isAccelerating = false;
+    int currentGear = 0;
+    float startSpeed;
+    float targetSpeed;
+    float gearTimer;
+    float gearUpTime;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        currentSpeed  = gearSpeeds[0];
+        StartGearUp();
     }
 
     // Input System の Move イベント
@@ -67,11 +92,83 @@ public class Player : MonoBehaviour
             );
         }
 
-        // 移動
-        if (movebutton)
+        // 加速・減速
+        if(movebutton || Input.GetKey(KeyCode.LeftShift))
         {
-            Vector3 move = transform.forward * moveSpeed * Time.fixedDeltaTime;
-            rb.MovePosition(rb.position + move);
+            if(!isAccelerating)
+            {
+                StartGearUp();
+                isAccelerating = true;
+            }
+
+            UpdateGearSpeed();
+        }
+        else
+        {
+            isAccelerating = false;
+            // ギアを下げる
+            if(currentGear > 0 && currentSpeed <= gearSpeeds[currentGear - 1])
+            {
+                currentGear--;
+                startSpeed = gearSpeeds[currentGear];
+                targetSpeed = gearSpeeds[Mathf.Min(currentGear + 1,gearSpeeds.Count - 1)];
+                gearTimer = 0.0f;
+                int debugGear = currentGear + 1;
+                Debug.Log("ギアが下がりました : " + debugGear);
+            }
+            // 原則処理　：　0に向かって減速
+            currentSpeed -= deceleration * Time.deltaTime;
+            // 最低速度で制限をかけて下回ったら停止する
+            currentSpeed = Mathf.Max(currentSpeed, 0);
+        }
+
+        // 移動
+        Vector3 move = transform.forward * currentSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + move);
+
+        //Debug.Log("ギア：　"　+ currentGear);
+    }
+
+    private void StartGearUp()
+    {
+        // 最高ギアでギアアップを止める
+        if (currentGear >= gearSpeeds.Count - 1) return;
+        // 次のギアへの初期化
+        startSpeed = currentSpeed;
+        targetSpeed = gearSpeeds[currentGear + 1];
+        gearUpTime = gearUpTimes[currentGear];
+        gearTimer = 0.0f; 
+    }
+
+    private void UpdateGearSpeed()
+    {
+        if (currentGear >= gearSpeeds.Count - 1)
+        {
+            currentSpeed = gearSpeeds[currentGear];
+             return;
+        }
+
+        gearTimer += Time.deltaTime; ;
+        // 各ギアのスピードアップ時間をtにコピー
+        float t = gearTimer / gearUpTime;
+        // ｔの値で線形補間
+        currentSpeed = Mathf.Lerp(startSpeed, targetSpeed, t);
+
+        if(t >= 1.0f)
+        {
+            currentGear++;
+            currentSpeed = gearSpeeds[currentGear];
+            StartGearUp();
+
+            int debugGear = currentGear + 1;
+            if (currentGear >= gearSpeeds.Count - 1)
+            {
+                Debug.Log("最高速度です : " + debugGear);
+            }
+            else
+            {
+                Debug.Log("ギアが上がりました : " + debugGear);
+            }
         }
     }
 }
